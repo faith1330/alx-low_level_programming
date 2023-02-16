b4ef13f7bbff539e338805dcadb954a7ff952b12
“Faith <faithiseh1330@gmail.com>
2023-02-16 12:41:51 -0800

    quotes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,8 +3,10 @@
 
 /**
  * main - Entry point
- * Return : Always (1)
+ * Description : A C program that prints a line to the standard error
+ * Return : 1 (Success)
  */
+
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
